ea811032dba9337a66cc8b6b9ec97149d80e8b43
sarahmathur <78476422+sarahmathur@users.noreply.github.com>
2021-11-22 16:44:58 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,52 @@
+
+img = "";
+objects = [];
+status = "";
+
+function preload(){
+  img = loadImage('https://image.shutterstock.com/image-photo/set-water-plastic-bottle-isolated-260nw-1033364095.jpg');
+}
+
+
+function setup() {
+  canvas = createCanvas(640, 420);
+  canvas.center();
+  objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+  document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function modelLoaded() {
+  console.log("Model Loaded!")
+  status = true;
+  objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results) {
+  if (error) {
+    console.log(error);
+  }
+  console.log(results);
+  objects = results;
+}
+
+
+function draw() {
+  image(img, 0, 0, 640, 420);
+
+      if(status != "")
+      {
+        for (i = 0; i < objects.length; i++) {
+          document.getElementById("status").innerHTML = "Status : Object Detected";
+    
+          fill("#FF0000");
+          percent = floor(objects[i].confidence * 100);
+          text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+          noFill();
+          stroke("#FF0000");
+          rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+      }
+}
+
+
+
